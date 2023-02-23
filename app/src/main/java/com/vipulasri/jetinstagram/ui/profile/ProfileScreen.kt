57851c530fbdc7e3fbf9e0e8b3fe9f762591c6d4
gguij004 <51983923+gguij004@users.noreply.github.com>
2023-02-23 10:18:17 -0500

    second

--- app/src/main/java/com/vipulasri/jetinstagram/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/profile/ProfileScreen.kt
@@ -1,28 +1,24 @@
 package com.vipulasri.jetinstagram.ui.profile
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.vipulasri.jetinstagram.data.PostsRepository
 import com.vipulasri.jetinstagram.data.StoriesRepository
-import com.vipulasri.jetinstagram.model.Post
+import com.vipulasri.jetinstagram.ui.components.diagonalGradientBorder
 import com.vipulasri.jetinstagram.ui.home.Post
 import com.vipulasri.jetinstagram.ui.home.StoriesList
 import com.vipulasri.jetinstagram.ui.profile.components.*
@@ -30,15 +26,16 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ProfileScreen(){
+fun ProfileScreen() {
     val scrollState = rememberScrollState()
     val stories by StoriesRepository.observeStories()
     val posts by PostsRepository.posts
     val coroutineScope = rememberCoroutineScope()
+    val itemSize: Dp = (LocalConfiguration.current.screenWidthDp.dp / 2)
     Scaffold(
-        topBar = {ProfileToolbar()}
+        topBar = { ProfileToolbar() }
     ) {
-        LazyColumn{
+        LazyColumn {
             item {
                 ProfileFollowers()
                 Spacer(Modifier.height(12.dp))
@@ -46,14 +43,48 @@ fun ProfileScreen(){
                 Spacer(Modifier.height(12.dp))
                 StoriesList(stories)
                 Divider()
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                ) {
+                    Row(
+                    ) {
+                        posts.forEach { post ->
+                            Box(
+                                modifier = Modifier
+                                    .width(itemSize)
+                                    .height(200.dp)
+                                    .padding(horizontal = 6.dp)
+                            
+                            ) {
+                                Post(post,
+                                    onDoubleClick = {
+                                        coroutineScope.launch {
+                                            PostsRepository.performLike(post.id)
+                                        }
+                                    },
+                                    onLikeToggle = {
+                                        coroutineScope.launch {
+                                            PostsRepository.toggleLike(post.id)
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
         }
-       // ProfilePosts()
     }
-}}
+}
 
 
 @Preview
 @Composable
-fun ShowProfileScreen(){
+fun ShowProfileScreen() {
     ProfileScreen()
 }
+
