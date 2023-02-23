6a6139c2535aa7e000a74ad88cc33867f372024b
gguij004 <51983923+gguij004@users.noreply.github.com>
2023-02-23 10:19:08 -0500

    clean imports

--- app/src/main/java/com/vipulasri/jetinstagram/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/profile/ProfileScreen.kt
@@ -4,21 +4,18 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.vipulasri.jetinstagram.data.PostsRepository
 import com.vipulasri.jetinstagram.data.StoriesRepository
-import com.vipulasri.jetinstagram.ui.components.diagonalGradientBorder
 import com.vipulasri.jetinstagram.ui.home.Post
 import com.vipulasri.jetinstagram.ui.home.StoriesList
 import com.vipulasri.jetinstagram.ui.profile.components.*
@@ -55,7 +52,7 @@ fun ProfileScreen() {
                                     .width(itemSize)
                                     .height(200.dp)
                                     .padding(horizontal = 6.dp)
-                            
+
                             ) {
                                 Post(post,
                                     onDoubleClick = {
